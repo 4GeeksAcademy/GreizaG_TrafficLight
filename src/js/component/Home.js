897fff0c289441ebc29a8b493f37cd9c3cd0d7f0
Greiza García <159574736+GreizaG@users.noreply.github.com>
2024-04-14 04:44:10 +0000

    [Forma del semáforo y código] Falta configurar el botón para que alumbre aleatoriamente. cada color

---
+++ src/js/component/Home.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+
+//create your first component
+const Home = () => {
+
+	const [selectedColor, setSelecterColor] = useState("red");
+	let color = ["red", "yellow", "green"];
+	let colorIndex = Math.floor(Math.random() * color.length);
+
+	return (
+
+		<div className="container d-flex align-item-center justify-content-center flex-column text-center mt-5">
+			<div className="stick justify-content-center"></div>
+			<div className="semaphore">
+				<div
+					onClick={() => setSelecterColor("red")}
+					className={"redLight" + (selectedColor === "red" ? " glow" : "")}></div>
+				<div
+					onClick={() => setSelecterColor("yellow")}
+					className={"yellowLight" + (selectedColor === "yellow" ? " glow" : "")}></div>
+				<div
+					onClick={() => setSelecterColor("green")}
+					className={"greenLight" + (selectedColor === "green" ? " glow" : "")}></div>
+			</div>
+			<button
+				onClick={() => setSelecterColor(color[colorIndex])}
+				className="btn btn-outline-secondary btn-sm mt-5">Random Color!</button>
+		</div >
+	);
+};
+
+export default Home;
